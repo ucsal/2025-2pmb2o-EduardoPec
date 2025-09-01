6baac13ced2c223b5d9157d71c787f0ea483dd3f
EduardoPec <eduardopecanha05@gmail.com>
2025-09-01 17:41:41 -0300

    Implementacao da atividade

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,9 @@ import br.com.mariojp.solid.ocp.*;
 
 public class Main {
 	public static void main(String[] args) {
+
 		DiscountCalculator calc = new DiscountCalculator();
+
 		System.out.println("REGULAR 100 -> " + calc.apply(100, CustomerType.REGULAR));
 		System.out.println("PREMIUM 100 -> " + calc.apply(100, CustomerType.PREMIUM));
 		// PARTNER deveria ter 12%
